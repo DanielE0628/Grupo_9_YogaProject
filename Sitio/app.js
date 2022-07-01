2b0bc13ddeac85092904831896ea049ba0ee316f
xHanzza <cortegosogonzalo@gmail.com>
2022-07-01 19:55:13 -0300

    subo varios htmls y estructura basica del public

--- Sitio/app.js
+++ Sitio/app.js
@@ -1,17 +1,20 @@
-const express = require ("express");
-const path = require('path');
-
+const express = require("express");
 const app = express();
 
+const path = require("path");
 const publicPath = path.join(__dirname, 'public');
-app.use( express.static(publicPath) );
 
-app.listen( 3000, () => console.log('Servidor corriendo') );
+app.use(express.static(publicPath))
 
-app.get('/404', (req, res) => {
-    res.send("Error página no encontrada");
-});
+app.listen(3100, ()=> {console.log("El servidor esta VIVO")});
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '/views/home.html'));
-});
+app.get("/", (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/home.html"))
+})
+
+app.get("/detalle-productos", (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/detalle-productos.html"))
+})
+app.get("/productos", (req,res)=>{
+    res.sendFile(path.resolve(__dirname,"./views/productos.html"))
+})
