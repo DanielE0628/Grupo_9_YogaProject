dc664bcbd58313787c167074061186414a3a3c60
Araya_Pablo <dsalattino@gmail.com>
2022-07-05 20:56:22 -0300

    modifico archivos html

--- Sitio/app.js
+++ Sitio/app.js
@@ -10,6 +10,10 @@ app.use(express.static(publicPath));
 
 app.listen( port, () => console.log(`Servidor corriendo in port ${port}`) );
 
+app.get('/404', (req, res) => {
+    res.send("Error página no encontrada");
+});
+
 app.get("/", (req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/home.html"))
 });
@@ -20,4 +24,12 @@ app.get("/detalle-productos", (req,res)=>{
 
 app.get("/productos", (req,res)=>{
     res.sendFile(path.resolve(__dirname,"./views/productos.html"))
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/register.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, '/views/login.html'));
 });
