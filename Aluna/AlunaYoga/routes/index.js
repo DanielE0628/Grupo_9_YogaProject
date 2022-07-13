f3fc259052db9d2b4e07604bf232d10af3e90be4
Brisa Vide <briviento@gmail.com>
2022-07-12 21:38:28 -0300

    agrego formato carpeta mvc

--- Aluna/AlunaYoga/routes/index.js
+++ Aluna/AlunaYoga/routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+const mainController = require("../controllers/main");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+router.get('/', mainController.home); 
+router.get('/productos', mainController.productos); 
+router.get('/detalle-productos', mainController.detalleProductos); 
+router.get('/login', mainController.login); 
+router.get('/register', mainController.register); 
+
 
 module.exports = router;
+
