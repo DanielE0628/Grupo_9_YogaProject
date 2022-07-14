fc6f572e4bdc881b5c112da5eb0104fa7d2c9516
Araya_Pablo <dsalattino@gmail.com>
2022-07-14 20:19:14 -0300

    version estable

--- controllers/main.js
+++ controllers/main.js
@@ -1,20 +1,29 @@
 const express = require ("express");
 
+const estilos = {
+    home: '/css/home-style.css',
+    productos: '/css/productos-style.css',
+    detlleProducto:'/css/detalle-producto-style.css',
+    register:'/css/register-style.css',
+    login:'/css/login-style.css',
+
+}
+
 const controlador = { 
     vistaHome: (req, res) => {
-        res.render('home',{title: 'Home'});
+        res.render('home',{title: 'Home', estilo: estilos.home});
     },
     productos: (req, res) => {
-        res.render('productos',{title: 'Productos'});
-    },
-    login: (req, res) => {
-        res.render('login',{title: 'Login'});
-    },
-    register: (req, res) => {
-        res.render('register', {title: 'Registro'});
+        res.render('productos',{title: 'Productos', estilo: estilos.productos});
     },
     detallesProductos: (req, res) => {
-        res.render('detalle-productos', {title: 'DetalleDeProductos'});
+        res.render('detalle-productos', {title: 'DetalleDeProductos', estilo: estilos.detalleProducto});
+    },
+    login: (req, res) => {
+        res.render('login',{title: 'Login', estilo: estilos.login});
+    },
+    register: (req, res) => {
+        res.render('register', {title: 'Registro', estilo: estilos.register});
     },
 };
 
