c4d12d5531157f9e0059a772fcc638a95c039b0f
Araya_Pablo <dsalattino@gmail.com>
2022-08-07 16:24:13 -0300

    se agrega pagina de error 404 not-found

--- src/app.js
+++ src/app.js
@@ -50,8 +50,16 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  if(err.status == 404){
+    res.status(404).render("not-found")
+  }else{
+    res.status(err.status || 500);
+    res.render('error');
+  }
+});
+
+app.use((req,res,next)=>{
+  
 });
 
 module.exports = app;
