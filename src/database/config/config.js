b6bc299a2692f509690faf8ed12a6c35cab83a02
xHanzza <cortegosogonzalo@gmail.com>
2022-09-24 18:11:47 -0300

    modelo crud con database

---
+++ src/database/config/config.js
@@ -0,0 +1,23 @@
+module.exports ={
+  "development": {
+    "username": "unnchgc0dzczkgt3",
+    "password": "hyo7SAvNwubFD4zkYIYV",
+    "database": "bmp1p9vlkc6khckqoedk",
+    "host": "bmp1p9vlkc6khckqoedk-mysql.services.clever-cloud.com",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
