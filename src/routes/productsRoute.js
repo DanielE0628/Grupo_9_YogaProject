c2d2b20eda646bd71d6402bc5a802fc97dcb5563
Gonzalo Cortegoso <106562120+xHanzza@users.noreply.github.com>
2022-09-28 12:02:01 -0300

    Update productsRoute.js
    se agrego el cartControler y las routes cart, por el momento están debilitadas

--- src/routes/productsRoute.js
+++ src/routes/productsRoute.js
@@ -8,16 +8,34 @@ const upload = require("../middlewares/productsMulterMiddleware")
 const validations = require("../middlewares/productsValidatorMiddleware");
 //Controller
 const productsController = require("../controllers/productsController");
+const cartController = require("../controllers/cartController");
 
 
-
-//Vistas
+//----------------Vistas-------------------------
 
 //todos los productos
 router.get('/', productsController.list);
 
 //un producto 
 router.get('/detail/:id/', productsController.detail); 
+//carrito
+//router.get('/cart/:id/', cartController.cartVista); 
+
+//-----------------Carrito----------------------
+
+//agregar
+router.post('/cart/add', cartController.add); 
+
+//borrar un prodcuto
+// router.delete('/cart/delete/:id/', cartController.destroy);
+
+//vaciar carrito
+// router.delete('/cart/deleteAll/:id/', cartController.destroyAll);
+
+//comprar
+//pendiente
+
+//------------------CRUD------------------------
 
 // //crear producto
 router.get('/create', productsController.create); 
