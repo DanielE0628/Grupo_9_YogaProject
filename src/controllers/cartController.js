d9f7fe69e02f1f842b86c5bb7c06e11b62452b49
Gonzalo Cortegoso <106562120+xHanzza@users.noreply.github.com>
2022-09-28 11:48:09 -0300

    Update cartController.js
    se agregaron controles ´por el momento no van a estar funcionales

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -6,9 +6,40 @@ const estilos = {
 
 
 const controlador ={
-    cartView:(req,res)=>{
-        res.render('products/cart',{title: 'CarritoDeCompras', estilo: estilos.carrito});
-    }
+    cartVista:(req,res)=>{
+        db.users.findByPk(req.params.id)
+        .then((userCart)=>{
+            res.render('products/cart',{userCart});//en la vista userCart.cart
+            })
+    },
+    cartAdd:(req, res)=>{
+        db.user.update(req.params.id)
+        .then((addProduct)=>{
+            db.user.cart =[];  // no va a funcionar es la idea, buscar manera de hacerlo
+            cart.push(addproduct)
+            return db.user.cart
+            res.render('products/cart',{addProduct});
+            })
+    },
+    //borrar producto del carrito
+    cartDestroy:(req, res)=>{
+        db.user.update(req.params.id)
+        .then((deleteProduct)=>{
+            db.user.cart =[];  // no va a funcionar es la idea, buscar manera de hacerlo
+            cart.pop(deleteProduct)
+            return db.user.cart
+            res.render('products/cart',{deleteProduct});
+            })
+    }, 
+   
+    //vaciar carrito
+       cartDestroyAll:(req, res)=>{
+        db.user.cart.delete(req.params.id)
+        .then((deleteAllProducts)=>{
+              // no va a funcionar es la idea, buscar manera de hacerlo
+            
+            res.render('products/cart',{deleteAllProducts});
+            })
 };
 
 module.exports = controlador;
